cfff1a0dcf62b6b721eb961855cb6edc3d2dd529
Nashrullah <60642964+nashrull13@users.noreply.github.com>
2023-10-05 00:51:16 +0700

    Update db_hbd.sql

--- db_hbd.sql
+++ db_hbd.sql
@@ -1,19 +1,106 @@
-CREATE DATABASE db_hbd;
-USE db_hbd;
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 04, 2023 at 07:49 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.14
 
-CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  firstName VARCHAR(255),
-  lastName VARCHAR(255),
-  birthday DATE,
-  email VARCHAR(255),
-  location VARCHAR(255)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE pending_messages (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  userId INT,
-  message TEXT,
-  sent BOOLEAN DEFAULT 0,
-  FOREIGN KEY (userId) REFERENCES users(id)
-);
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_hbd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pending_messages`
+--
+
+CREATE TABLE `pending_messages` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `sent` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstName`, `lastName`, `birthday`, `email`, `location`) VALUES
+(1, 'Aceng', 'Juanda', '2023-10-05', 'acengjuanda48@gmail.com', 'Auckland'),
+(2, 'Asep', 'Sutrisna', '2023-10-06', 'sutrisnaasep848@gmail.com', 'Jakarta');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pending_messages`
+--
+ALTER TABLE `pending_messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pending_messages`
+--
+ALTER TABLE `pending_messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pending_messages`
+--
+ALTER TABLE `pending_messages`
+  ADD CONSTRAINT `pending_messages_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
